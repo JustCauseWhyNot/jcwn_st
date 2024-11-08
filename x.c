26bf3c2413b549aa6bece6f8a8e0943c72af57eb
JustCauseWhyNot <JustCauseWhyNot@tuta.io>
2024-11-07 21:01:30 -0700

    feat: apply default cursor patch

--- x.c
+++ x.c
@@ -1864,7 +1864,7 @@ xsetcursor(int cursor)
 {
 	if (!BETWEEN(cursor, 0, 7)) /* 7: st extension */
 		return 1;
-	win.cursor = cursor;
+	win.cursor = (cursor ? cursor : cursorshape);
 	return 0;
 }
 
